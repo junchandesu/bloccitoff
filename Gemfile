aaedaf448ba07a003a76040114831cf338846e36
Junko Werner <aliciajace@gmail.com>
2015-06-25 20:46:38 -0700

    Seed with Fakler

--- Gemfile
+++ Gemfile
@@ -29,6 +29,8 @@ gem 'figaro', '1.0'
 
 gem 'bootstrap-sass'
 
+gem 'faker'
+
 # Use ActiveModel has_secure_password
 # gem 'bcrypt', '~> 3.1.7'
 
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,40 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+require 'faker'
+
+5.times do  
+	user = User.new(
+		name: Faker::Name.name,
+		email: Faker::Internet.email,
+		password: Faker::Internet.password(8)
+		)
+	user.skip_confirmation!
+	user.save!
+end
+
+	user = User.new(
+		name: "Junjun",
+		email: "aliciajace@gmail.com",
+		password: "alicia16"
+	)
+	user.skip_confirmation!
+	user.save!
+
+	users = User.all
+
+50.times do   
+	Item.create!(
+		user: users.sample,
+		name: Faker::Hacker.say_something_smart
+		)
+end
+
+admin = User.new(
+	name: "Admin User",
+	email: 'admin@exmaple.com',
+	password: 'helloworld',
+	)
+admin.skip_confirmation!
+admin.save!
+
+puts "Seed finished"
+puts "#{User.count} users created"
+puts "#{Item.count} items created" 
